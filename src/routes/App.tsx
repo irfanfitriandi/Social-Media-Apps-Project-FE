ee18c496b862d7d8738eded34c03f8e5270b2af3
Irfan Fitriandi <irfanfitriandi@gmail.com>
2023-01-16 10:59:01 +0700

    React TS Template

---
+++ src/routes/App.tsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+function App() {
+  return (
+    <div>
+      <p>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Expedita cupiditate, necessitatibus consectetur, voluptatem ab animi reiciendis fuga earum esse amet incidunt accusantium officia nesciunt est minus hic facilis quo a.
+      </p>
+    </div>
+  );
+}
+
+export default App;
