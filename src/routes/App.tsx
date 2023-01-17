cfed577336fceecf40a261f28e094ee9e5eaec46
wildanmujahidin <wildan.mujahidin1@gmail.com>
2023-01-17 15:11:01 +0700

    fetch: add routing all pages in App.tsx

--- src/routes/App.tsx
+++ src/routes/App.tsx
@@ -1,4 +1,5 @@
 import React from "react";
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
 
 import Home from "../pages/Home";
 import Login from "../pages/Login";
@@ -8,12 +9,39 @@ import Profile from "../pages/Profile";
 import Search from "../pages/Search";
 import Settings from "../pages/Settings";
 
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <Home />,
+  },
+  {
+    path: "/login",
+    element: <Login />,
+  },
+  {
+    path: "/signup",
+    element: <Signup />,
+  },
+  {
+    path: "/detail-post/:id_post",
+    element: <DetailPost />,
+  },
+  {
+    path: "/profile/:id_user",
+    element: <Profile />,
+  },
+  {
+    path: "/search",
+    element: <Search />,
+  },
+  {
+    path: "/settings/:id_user",
+    element: <Settings />,
+  },
+]);
+
 function App() {
-  return (
-    <div>
-      <Home />
-    </div>
-  );
+  return <RouterProvider router={router} />;
 }
 
 export default App;
